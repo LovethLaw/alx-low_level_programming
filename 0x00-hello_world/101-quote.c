9d8e0901549ab3a6b2f3a040a3180f77bd33426c
lovethlaw <talktoawi@gmail.com>
2023-08-10 16:27:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
  * Return: always 1 (success)
@@ -9,4 +8,4 @@ int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
-}	
+}
