4ac66a5cc97ff6e06f06650a46146dea3f9fd56d
lovethlaw <talktoawi@gmail.com>
2023-08-11 21:11:16 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
- * main - main function
- * Return: always 0
+ * main - print numbers to stdout
+ * Desciption: print the last digits of random numbers
+ * Return: always 0 (successful)
  */
 int main(void)
 {
-	char i;
+	int n, lastNum;
 
-	for (i = 'a' ; i <= 'Z' ; i++)
-		putchar(i);
-	putchar('\n');
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	lastNum = n % 10;
+	if (lastNum > 5)
+	{
+		printf("last digit of %d is %d and is greater than 5\n", n, lastNum);
+	} else if (lastNum < 6 && lastNum != 0)
+	{
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
+	} else
+	{
+		printf("lastNum digit of %d is %d and is 0\n", n, lastNum);
+	}
 	return (0);
 }
