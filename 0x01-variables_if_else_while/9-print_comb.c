4b7ce65d0c042b4ecb404f7a9f6e413310a3e794
lovethlaw <talktoawi@gmail.com>
2023-08-11 20:04:12 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers to stdout
+ * Description: prints all possible combinations
+ * Return: always return 0 (successfull)
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers <= 9; numbers++)
+	{
+		putchar('0' + numbers);
+		if (numbers != 9)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
