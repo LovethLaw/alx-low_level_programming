41c11102d2942e26523e877fb350f440adc8f274
lovethlaw <talktoawi@gmail.com>
2023-08-28 12:28:19 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - Gets the length of a profix substring.
+ * @s: The string to be searched.
+ * @accept: The profix to be measured.
+ *
+ * Return: The number of bytes in s which consist only of bytes from accepts.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int index;
+
+	while (*s)
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+	s++;
+}
