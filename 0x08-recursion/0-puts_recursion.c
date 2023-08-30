f2b1462aba51ccc684c21b7debdf09b0b832ccdc
lovethlaw <talktoawi@gmail.com>
2023-08-30 12:53:41 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: input string to be printed
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
